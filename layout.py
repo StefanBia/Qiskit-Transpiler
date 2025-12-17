cfabfa84a48bb46be9cf1efe102e1005b9adcddb
Stefan Bia <stefanbia0108@gmail.com>
2025-12-17 13:50:58 +0200

    Create fake backend to test layout. Change circuit to graph to store indexes instead of Qubit classes as entries in adj list

--- layout.py
+++ layout.py
@@ -4,6 +4,7 @@ import networkx as nx  # optional, easier layout
 
 
 class Layout:
+    @staticmethod
     def draw_graph(graph):
         G = nx.Graph()
         # Add nodes with labels
@@ -18,15 +19,27 @@ class Layout:
         nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1000, font_size=12, font_weight='bold')
         plt.show()
 
+    @staticmethod
     def run_layout(qc, backend):
-        graph = Graph()
+        G1 = Graph()
+        G2 = Graph()
 
+        for i in range (backend.num_qubits):
+            G2.add_node(i)
+        
+        for x, y in backend.coupling_map:
+            G2.add_edge(x, y)
         # Add all qubits as nodes
         for q in qc.qubits:
-            graph.add_node(q, label=str(q))
+            idx, _ = qc.find_bit(q)
+            G1.add_node(idx)
 
         # Add edges for every 2-qubit gate
         for instr, qargs, cargs in qc.data:
             if len(qargs) == 2:  # two-qubit gate
                 q0, q1 = qargs
-                graph.add_edge(q0, q1)
+                idx0, _ = qc.find_bit(q0)
+                idx1, _ = qc.find_bit(q1)
+                G1.add_edge(idx0, idx1)
+        
+        return find_subgraph_match(G1, G2)
